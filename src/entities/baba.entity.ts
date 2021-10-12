c0699eb8e20f56ee0a4bf21eef0c0e98cddd1caf
Gabriel Dias <dias.1618@gmail.com>
2021-10-11 22:56:36 -0300

    fix(baba): Retirada de data do baba

--- src/entities/baba.entity.ts
+++ src/entities/baba.entity.ts
@@ -6,20 +6,15 @@ import { Encontro } from "./encontro.entity";
 export class Baba extends BaseEntity{
 
     
-    constructor(data?: {id?:number, dtBaba?:Date, nmBaba:string}){
+    constructor(data?: {id?:number, nmBaba:string}){
         super();
         this.id = data && data.id || 0;
-        this.dtBaba = data && data.dtBaba || null;
         this.nmBaba = data && data.nmBaba || null;
     }
 
     @PrimaryGeneratedColumn()
     id:number;
 
-    @ApiProperty({ example: '2021-09-30', description: 'Data do Baba' })
-    @Column("timestamp without time zone", {nullable: true})
-    dtBaba:Date;
-    
     @ApiProperty({ example: 3, description: 'Nome do baba' })
     @Column("character varying", {nullable: true})
     nmBaba:string;
